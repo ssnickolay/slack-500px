6d640605a8893fcac5e620314981132c45067bfa
ssnikolay <ssnikolay@gmail.com>
2015-10-02 22:46:14 +0300

    Slack500px#black_and_white

--- slack_500px/finder.rb
+++ slack_500px/finder.rb
@@ -3,12 +3,14 @@ require 'multi_json'
 require_relative 'auth_service'
 require_relative 'request/base'
 require_relative 'request/popular'
+require_relative 'request/black_and_white'
 require_relative 'request/search'
 
 module Slack500px
   class Finder
     RESERVED = {
-      'popular' => Slack500px::Request::Popular
+      'popular' => Slack500px::Request::Popular,
+      'black_and_white' => Slack500px::Request::BlackAndWhite
     }
 
     def initialize(query, _)
---
+++ slack_500px/request/black_and_white.rb
@@ -0,0 +1,11 @@
+module Slack500px
+  module Request
+    class BlackAndWhite < Base
+      private
+
+      def build_url
+        'https://api.500px.com/v1/photos?feature=popular&sort=%{sort}&rpp=1&image_size=4&page=%{page}&only=Black and White' % params
+      end
+    end
+  end
+end
